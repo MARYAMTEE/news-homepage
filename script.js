846dce4aec53534c850d94a4769e9dac336eadc2
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-06-21 15:52:25 +0100

    update overlay

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const navigation = document.querySelector("nav");
+const overlay = document.querySelector(".overlay");
+
 
 openBtn.addEventListener("click", ()=> {
     navigation.classList.toggle("toggle")
+    overlay.style.display="block";
 });
 
 closeBtn.addEventListener("click", ()=> {
     navigation.classList.remove("toggle");
+    overlay.style.display="none";
 })
+
+
