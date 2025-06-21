7bc023d572c345cfad98bc26e5a74119f5398f09
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-06-21 16:17:30 +0100

    update nav bar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const navigation = document.querySelector("nav");
 const overlay = document.querySelector(".overlay");
+const navLink = document.querySelectorAll(".nav-link");
 
 
 openBtn.addEventListener("click", ()=> {
@@ -12,6 +13,20 @@ openBtn.addEventListener("click", ()=> {
 closeBtn.addEventListener("click", ()=> {
     navigation.classList.remove("toggle");
     overlay.style.display="none";
+});
+
+// Close nav bar when the overlay is clicked
+overlay.addEventListener("click", ()=> {
+    navigation.classList.remove("toggle");
+    overlay.style.display="none";
 })
+
+navLink.forEach(link =>{
+    link.addEventListener("click", ()=> {
+    navigation.classList.remove("toggle");
+    overlay.style.display="none";
+})
+})
+// Close the navbar a the list item is clicked
 
 
